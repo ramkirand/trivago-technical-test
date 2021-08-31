ce76b40fdcaf99641097975a57872ba1112bc526
D Rama Kiron <37409052+ramkirand@users.noreply.github.com>
2021-08-31 22:28:55 +0530

    updated

--- src/main/java/com/trivago/Application.java
+++ src/main/java/com/trivago/Application.java
@@ -3,10 +3,10 @@ package com.trivago;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication public class Application {
+@SpringBootApplication
+public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
