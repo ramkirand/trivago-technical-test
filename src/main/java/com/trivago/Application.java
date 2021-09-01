ba5d156ac306cc50c6c5851279392b89413e50e8
D Rama Kiron <37409052+ramkirand@users.noreply.github.com>
2021-09-01 08:47:24 +0530

    Updated build.gradle

--- src/main/java/com/trivago/Application.java
+++ src/main/java/com/trivago/Application.java
@@ -2,10 +2,8 @@ package com.trivago;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class Application {
 
   public static void main(String[] args) {
