e7455d15fdec86d7613753b52c790d66b3b7e248
D Rama Kiron <37409052+ramkirand@users.noreply.github.com>
2021-09-08 11:15:38 +0530

    Update HotelSearchEngineImpl.java
    minor change

--- src/main/java/com/trivago/service/HotelSearchEngineImpl.java
+++ src/main/java/com/trivago/service/HotelSearchEngineImpl.java
@@ -12,88 +12,84 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class HotelSearchEngineImpl implements HotelSearchEngine {
-  private static final String SEARCH_CACHE = "searchCache";
-  private final AtomicInteger queryCount = new AtomicInteger(0);
-  private final DataProcessorService dataProcessorService;
-  private final DataReaderService dataReaderService;
+    private static final String SEARCH_CACHE = "searchCache";
+    private final AtomicInteger queryCount = new AtomicInteger(0);
+    private final DataProcessorService dataProcessorService;
+    private final DataReaderService dataReaderService;
 
-  @Autowired
-  HotelSearchEngineImpl(
-      DataProcessorService dataProcessorService, DataReaderService dataReaderService) {
-    this.dataProcessorService = dataProcessorService;
-    this.dataReaderService = dataReaderService;
-  }
-
-  @Override
-  public void initialize() {
-    dataReaderService.readCsvData();
-    dataProcessorService.processCityData();
-    dataProcessorService.processHotelData();
-    dataProcessorService.processAdvertiserIdWithHotels();
-  }
-
-  @Override
-  @TrackExecutionTime
-  @Cacheable(value = SEARCH_CACHE, key = "#cityName")
-  public List<HotelWithOffers> performSearch(
-      String cityName, DateRange dateRange, OfferProvider offerProvider) {
-    Integer cityId = Storage.cityIdMap.get(cityName);
-    List<Hotel> allHotelsInCurrentCity =
-        Storage.hotelInGivenCity.getOrDefault(cityId, new ArrayList<>());
-
-    List<Integer> listHotelIdWithOffer = new ArrayList<>();
-    for (Hotel hotel : allHotelsInCurrentCity) {
-      Integer currentHotel = hotel.getId();
-      if (!Storage.allHotelIdWithOfferSet.contains(currentHotel)) {
-        continue;
-      }
-
-      if (!Storage.hotelIdToHotelMap.containsKey(currentHotel)) {
-        continue;
-      }
-      listHotelIdWithOffer.add(hotel.getId());
+    @Autowired
+    HotelSearchEngineImpl(
+            DataProcessorService dataProcessorService, DataReaderService dataReaderService) {
+        this.dataProcessorService = dataProcessorService;
+        this.dataReaderService = dataReaderService;
     }
 
-    Storage.advertiserToListHotelId.forEach(
-        (k, v) -> {
-          try {
-            Map<Integer, Offer> hotelsToOfferFromThisAdvertiser =
-                offerProvider.getOffersFromAdvertiser(k, listHotelIdWithOffer, dateRange);
-            hotelsToOfferFromThisAdvertiser.forEach(
-                (key, val) -> {
-                  List<Offer> offers =
-                      Storage.hotelIdToOffersList.getOrDefault(key, new ArrayList<>());
-                  offers.add(val);
-                  Storage.hotelIdToOffersList.put(key, offers);
+    @Override
+    public void initialize() {
+        dataReaderService.readCsvData();
+        dataProcessorService.processCityData();
+        dataProcessorService.processHotelData();
+        dataProcessorService.processAdvertiserIdWithHotels();
+    }
+
+    @Override
+    @TrackExecutionTime
+    @Cacheable(value = SEARCH_CACHE, key = "#cityName")
+    public List<HotelWithOffers> performSearch(
+            String cityName, DateRange dateRange, OfferProvider offerProvider) {
+        Integer cityId = Storage.cityIdMap.get(cityName);
+        List<Hotel> allHotelsInCurrentCity =
+                Storage.hotelInGivenCity.getOrDefault(cityId, new ArrayList<>());
+
+        List<Integer> listHotelIdWithOffer = new ArrayList<>();
+        for (Hotel hotel : allHotelsInCurrentCity) {
+            Integer currentHotel = hotel.getId();
+            if (!Storage.allHotelIdWithOfferSet.contains(currentHotel)) {
+                continue;
+            }
+
+            if (!Storage.hotelIdToHotelMap.containsKey(currentHotel)) {
+                continue;
+            }
+            listHotelIdWithOffer.add(hotel.getId());
+        }
+
+        Storage.advertiserToListHotelId.forEach(
+                (k, v) -> {
+                    try {
+                        Map<Integer, Offer> hotelsToOfferFromThisAdvertiser =
+                                offerProvider.getOffersFromAdvertiser(k, listHotelIdWithOffer, dateRange);
+                        hotelsToOfferFromThisAdvertiser.forEach(
+                                (key, val) -> {
+                                    List<Offer> offers =
+                                            Storage.hotelIdToOffersList.getOrDefault(key, new ArrayList<>());
+                                    offers.add(val);
+                                    Storage.hotelIdToOffersList.put(key, offers);
+                                });
+
+                    } catch (IOException e) {
+                        log.debug(e.getMessage());
+                    }
                 });
 
-          } catch (IOException e) {
-            log.debug(e.getMessage());
-          }
-        });
+        List<HotelWithOffers> listHotelWithOffers = new ArrayList<>();
+        listHotelIdWithOffer.forEach(l ->listHotelWithOffers.add(
+                                new HotelWithOffers(
+                                        Storage.hotelIdToHotelMap.get(l), Storage.hotelIdToOffersList.get(l))));
+        queryCount.getAndIncrement();
+        listHotelWithOffers.sort(Comparator.comparingInt(o -> o.getHotel().getId()));
+        return listHotelWithOffers;
+    }
 
-    List<HotelWithOffers> listHotelWithOffers = new ArrayList<>();
-    listHotelIdWithOffer.forEach(
-        l ->
-            listHotelWithOffers.add(
-                new HotelWithOffers(
-                    Storage.hotelIdToHotelMap.get(l), Storage.hotelIdToOffersList.get(l))));
-    queryCount.getAndIncrement();
-    return listHotelWithOffers.stream().sorted().collect(Collectors.toList());
-  }
-  @Override
-  public synchronized long totalQueries() {
-    log.info("<<<<<<<<<<   Total Queries: " + queryCount.get());
-    return queryCount.get();
-  }
+    @Override
+    public synchronized long totalQueries() {
+        log.info("<<<<<<<<<<   Total Queries: " + queryCount.get());
+        return queryCount.get();
+    }
 }
